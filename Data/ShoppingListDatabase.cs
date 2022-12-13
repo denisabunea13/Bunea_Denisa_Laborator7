23cd1df6101ab390b4a33644340615d6de67e081
Denisa Bunea <denibunea1311@gmail.com>
2022-12-13 16:02:44 +0200

    lab9

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -19,6 +19,32 @@ namespace Bunea_Denisa_Laborator7.Data
             _database.CreateTableAsync<ListProduct>().Wait();
 
         }
+        public Task<List<ShopList>> GetShopListsAsync()
+        {
+            return _database.Table<ShopList>().ToListAsync();
+        }
+        public Task<ShopList> GetShopListAsync(int id)
+        {
+            return _database.Table<ShopList>()
+            .Where(i => i.ID == id)
+           .FirstOrDefaultAsync();
+        }
+        public Task<int> SaveShopListAsync(ShopList slist)
+        {
+            if (slist.ID != 0)
+            {
+                return _database.UpdateAsync(slist);
+            }
+            else
+            {
+                return _database.InsertAsync(slist);
+            }
+        }
+        public Task<int> DeleteShopListAsync(ShopList slist)
+        {
+            return _database.DeleteAsync(slist);
+        }
+
         public Task<int> SaveProductAsync(Product product)
         {
             if (product.ID != 0)
@@ -37,6 +63,26 @@ namespace Bunea_Denisa_Laborator7.Data
         public Task<List<Product>> GetProductsAsync()
         {
             return _database.Table<Product>().ToListAsync();
+        }
+
+        public Task<int> SaveListProductAsync(ListProduct listp)
+        {
+            if (listp.ID != 0)
+            {
+                return _database.UpdateAsync(listp);
+            }
+            else
+            {
+                return _database.InsertAsync(listp);
+            }
+        }
+        public Task<List<Product>> GetListProductsAsync(int shoplistid)
+        {
+            return _database.QueryAsync<Product>(
+            "select P.ID, P.Description from Product P"
+            + " inner join ListProduct LP"
+            + " on P.ID = LP.ProductID where LP.ShopListID = ?",
+            shoplistid);
         }
     }
 }
